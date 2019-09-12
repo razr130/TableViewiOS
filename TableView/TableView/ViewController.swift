dd585527bf83caf8b0da4d1771d5fcb18c047181
SpaceColony <jovfernando@yahoo.com>
2019-09-12 11:24:42 +0700

    first commit

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Johanes Vincent Fernando on 12/09/19.
+//  Copyright © 2019 SpaceColony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
